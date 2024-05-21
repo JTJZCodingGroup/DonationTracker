fe488e439591a76005702379d7ec9c3a710071ec
Jesse Wowczuk <jessewowczuk@gmail.com>
2024-05-21 10:24:48 -0700

    created Navibar

--- donation-tracker/src/app/components/Navbar.tsx
+++ donation-tracker/src/app/components/Navbar.tsx
@@ -1,1 +1,48 @@
 // create links for login, proejcts, donate, create project
+"use client";
+
+import Link from 'next/link';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-black p-4">
+      <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
+        <div className="text-white font-bold text-3xl mb-4 lg:mb-0 hover:text-orange-600 hover:cursor-pointer">
+          Donation Tracker
+        </div>
+
+        <div className="lg:hidden">
+          <button className="text-white focus:outline-none">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+          </button>
+        </div>
+
+        <div className="lg:flex flex-col lg:flex-row lg:space-x-4 lg:mt-0 mt-4 flex flex-col items-center text-xl">
+          <Link href="/projects">
+            <button className="text-white px-4 py-2 hover:text-orange-600 focus:outline-none">Projects</button>
+          </Link>
+          <Link href="/donate">
+            <button className="text-white px-4 py-2 hover:text-orange-600 focus:outline-none">Donate</button>
+          </Link>
+          <Link href="/login">
+            <button className="text-white px-4 py-2 hover:text-orange-600 focus:outline-none">Login</button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+
+
+
+
--- donation-tracker/src/app/page.tsx
+++ donation-tracker/src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import Navbar from './components/Navbar';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page</h1>
-      <h3>Check out our projects!</h3>
-    </main>
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold mb-4">Home Page</h1>
+        <h3 className="text-lg">Check out our projects!</h3>
+      </main>
+    </div>
   );
 }
